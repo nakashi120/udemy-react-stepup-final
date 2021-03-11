2b71d8d529667fccaaf1094a0e047f220e329be2
nakashi120 <nakashi120@gmail.com>
2021-03-11 23:41:08 +0900

    setting router

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
+// 外部から
 import React from "react"
-import { Button, ChakraProvider } from "@chakra-ui/react"
+import { ChakraProvider } from "@chakra-ui/react"
+import { BrowserRouter } from "react-router-dom"
+
+// 内部から
 import theme from "./theme/theme"
+import { Router } from "./router/Router"
 
 function App() {
   return (
     <div className="App">
       <ChakraProvider theme={theme}>
-        <Button colorScheme="teal">ボタン</Button>
-        <p>sample</p>
+        <BrowserRouter>
+          <Router />
+        </BrowserRouter>
       </ChakraProvider>
     </div>
   )
